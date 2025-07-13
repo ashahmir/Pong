d94616b8387b5b15891e8feebf2696a5d904ae16
Ghulam Latif <141661326+ashahmir@users.noreply.github.com>
2025-07-13 21:59:15 +0500

    Add files via upload

---
+++ score.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self, player):
+        self.y = 0
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.score = 0
+        if player == 1:
+            self.goto(-100, 230)
+        else:
+            self.goto(100,230)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"{self.score}", align="center", font=("Michroma", 40, "normal"))
+
+    def match_win(self, player):
+        self.goto(0,-20)
+        self.write(f"Player {player} won the match", align="center", font=("Michroma", 20, "normal"))
+
