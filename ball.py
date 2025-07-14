f46bd719bba10a5559e24bfd0158e8345d5404e0
Ghulam Latif <141661326+ashahmir@users.noreply.github.com>
2025-07-14 07:41:49 +0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -4,25 +4,24 @@ from turtle import Turtle
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.direction = 15
         self.angle = 150
         self.shape("circle")
         self.color("white")
-        self.setheading(self.angle)
+        self.penup()
+        self.move_x = 10
+        self.move_y = 10
 
     def move_ball(self):
-        self.penup()
-        self.forward(self.direction)
+        new_x = self.xcor() + self.move_x
+        new_y = self.ycor() + self.move_y
+        self.goto(new_x, new_y)
 
     def bounce_from_player(self):
-        self.direction *= -1
-        self.right(45)
+        self.move_x *= -1
 
     def bounce_from_walls(self):
         self.goto(0,0)
 
     def bounce_from_ground(self):
-        self.right(-135)
+        self.move_y *= -1
 
-    def bounce_from_top(self):
-        self.right(135)
